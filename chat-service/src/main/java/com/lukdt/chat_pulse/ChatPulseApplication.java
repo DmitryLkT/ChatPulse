3bf4c69a7f9882a8fed85b05b2b254a17b99c08c
DmitryTL <Dmitry.plus1@yandex.ru>
2026-01-11 13:59:36 +0600

    start

---
+++ chat-service/src/main/java/com/lukdt/chat_pulse/ChatPulseApplication.java
@@ -0,0 +1,13 @@
+package com.lukdt.chat_pulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatPulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatPulseApplication.class, args);
+	}
+
+}
---
+++ chat-service/src/test/java/com/lukdt/chat_pulse/ChatPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukdt.chat_pulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatPulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
