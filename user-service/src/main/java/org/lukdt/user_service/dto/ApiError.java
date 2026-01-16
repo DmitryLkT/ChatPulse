d4e52ad244a41139b4d6623ed9aa18203e327e38
DmitryTL <Dmitry.plus1@yandex.ru>
2026-01-16 14:04:06 +0600

    ati error dto

---
+++ user-service/src/main/java/org/lukdt/user_service/dto/ApiError.java
@@ -0,0 +1,9 @@
+package org.lukdt.user_service.dto;
+
+import java.time.LocalDateTime;
+
+public record ApiError(
+        int status,
+        String message,
+        LocalDateTime timestamp
+) {}
